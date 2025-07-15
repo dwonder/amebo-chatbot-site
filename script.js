958e303d3ffbc9c9380bdbe0f8e051e9842ce3e9
dwonder <dy_dayo@yahoo.com>
2025-07-15 17:16:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// This script is loaded by the website but is not required for the Dialogflow chatbot to function.
-// It can be used to add custom features in the future, like interactive elements or animations.
+// This script is loaded by the website but is not required for the current features to function.
+// All functionality (chatbot embed, video embed, image scroller) is handled by HTML and CSS.
+// You can use this file later to add more advanced interactivity.
 
-console.log("Amebo website script has loaded successfully.");```
+console.log("Amebo website script has loaded successfully.");
