546ac7b16409361a3d9ee50de619e616d621f667
dwonder <dy_dayo@yahoo.com>
2025-07-15 17:11:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This script is loaded by the website but is not required for the Dialogflow chatbot to function.
+// It can be used to add custom features in the future, like interactive elements or animations.
+
+console.log("Amebo website script has loaded successfully.");```
